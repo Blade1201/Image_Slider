be46da14a7c129992f564dfa29fab445566a4fdd
Blade1201 <molnar.aron1201@gmail.com>
2024-03-30 15:26:06 +0100

    minor fixes

--- src/App.js
+++ src/App.js
@@ -37,42 +37,42 @@ function App() {
     <div className="App">
        <div className="container">
       <div className="slide">
-        <div className="item" style={{backgroundImage: IMG1}}>
+        <div className="item" style={{backgroundImage: `url(${IMG1})`}}>
           <div className="content">
             <div className="name">Svájc</div>
             <div className="des">Svájc, hivatalos nevén Svájci Konföderáció, Svájci Államszövetség, Svájci Esküszövetség tengerparttal nem rendelkező, kantonokból álló szövetségi köztársaság Közép-Európában. Szomszédai Németország, Franciaország, Olaszország, Ausztria és Liechtenstein.</div>
             <button>Részletek</button>
           </div>
         </div>
-        <div className="item" style={{backgroundImage: IMG2}}>
+        <div className="item" style={{backgroundImage: `url(${IMG2})`}}>
           <div className="content">
             <div className="name">Finnország</div>
             <div className="des">Finnország észak-európai ország. Délnyugatról a Balti-tenger, délkeletről a Finn-öböl, nyugatról a Botteni-öböl határolja. Szárazföldön Svédországgal, Norvégiával és Oroszországgal határos, tengeren pedig Észtországgal. „Az ezer tó országaként” emlegetik, de a területén ennél sokkal több, mintegy 180000 tó található.</div>
             <button>Részletek</button>
           </div>
         </div>
-        <div className="item" style={{backgroundImage: IMG3}}>
+        <div className="item" style={{backgroundImage: `url(${IMG3})`}}>
           <div className="content">
             <div className="name">Izland</div>
             <div className="des">Izland szigetország az Atlanti-óceán északi részén, Grönland és Skócia között, Feröertől északnyugatra, a két globális jelentőségű tektonikai lemez – az eurázsiai és az észak-amerikai lemez határvonalán. Hivatalos nyelve az indoeurópai nyelvcsaládba tartozó izlandi nyelv. Fővárosa és legnépesebb városa Reykjavík.</div>
             <button>Részletek</button>
           </div>
         </div>
-        <div className="item" style={{backgroundImage: IMG4}}>
+        <div className="item" style={{backgroundImage: `url(${IMG4})`}}>
           <div className="content">
             <div className="name">Ausztrália</div>
             <div className="des">Ausztrália, hivatalos nevén az Ausztrál Államszövetség föderatív királyság, mely az ausztrál kontinenst, Tasmania szigetét és számos kisebb szigetet foglal magába. Északról Pápua Új-Guinea, Indonézia és Kelet-Timor; északkelet felől a Salamon-szigetek és Vanuatu; délkelet felől pedig Új-Zéland határolja tengeren. </div>
             <button>Részletek</button>
           </div>
         </div>
-        <div className="item" style={{backgroundImage: IMG5}}>
+        <div className="item" style={{backgroundImage: `url(${IMG5})`}}>
           <div className="content">
             <div className="name">Hollandia</div>
             <div className="des">Hollandia, hivatalosan a Holland Királyság északnyugat-európai ország, amely tengerentúli területekkel is rendelkezik a Karib-térségben. Európa egyik legsűrűbben lakott országa. Európában Hollandia tizenkét tartományból áll.</div>
             <button>Részletek</button>
           </div>
         </div>
-        <div className="item" style={{backgroundImage: IMG6}}>
+        <div className="item" style={{backgroundImage: `url(${IMG6})`}}>
           <div className="content">
             <div className="name">Írország</div>
             <div className="des">Írország vagy Ír Köztársaság Európa északnyugati részén és harmadik legnagyobb szigetén, az Ír-szigeten található állam. A sziget északi része a brit korona fennhatósága alá tartozik, az Egyesült Királyság része, ezt nevezik Észak-Írországnak. A sziget többi része a tulajdonképpeni Írország. </div>
